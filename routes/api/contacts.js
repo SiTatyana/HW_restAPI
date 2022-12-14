5e651d7c560af353dda38808c49beb8f6084365e
Siyanko Tetiana <sisenorawork@gmail.com>
2022-12-14 16:12:07 +0000

    add other methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,67 +1,14 @@
 const express = require('express');
-const Joi = require("joi");
 
-const contacts = require("../../models/contacts")
-
-const {HttpError} = require("../../helpers")
+const ctrl = require("../../controllers/contacts")
 
 const router = express.Router()
 
-const addShema = Joi.object({
-  name: Joi.string().required(),
-  phone: Joi.string().required(),
-  email: Joi.string().required(),
-})
 
-router.get('/', async (req, res, next) => {
-  try{
-  const result = await contacts.listContacts();
-  res.json(result);
-  }
-  catch(error){
-    // next(error);
-    res.status(500).json({
-      message:"Server error"
-    })
-  }
-  
-})
-
-router.get('/:contactId', async (req, res, next) => {
-    try {
-      const {contactId} = req.params;
-      const result = await contacts.getContactById(contactId);
-
-      if(!result) {
-       throw HttpError(404, "Not found");
-      }
-      res.json(result);
-    }
-    catch(error){
-      next(error);
-    }
-})
-
-router.post('/', async (req, res, next) => {
-  try {
-    const {error} = addShema.validate(req.body);
-    if(error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  }
-  catch(error){
-    next(error);
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrl.listContacts)
+router.get('/:contactId', ctrl.getContactById)
+router.post('/', ctrl.addContact)
+router.delete('/:contactId', ctrl.removeContact)
+router.put('/:contactId', ctrl.updateById)
 
 module.exports = router
